fc207dcb392c8abb5fa16c200152a2491b69f60f
Scott Zachariae <scott.zachariae@ibykus.de>
2024-06-18 16:02:36 +0200

    first commit

---
+++ blocker.js
@@ -0,0 +1,16 @@
+let elements = document.getElementsByClassName("SoAPf")
+
+// console.log(elements[0])
+
+for (var i = 0; i < elements.length; i++) {
+  // console.log(elements[i])
+
+  if (elements[i].innerText.includes("ringer Allgemeine")) {
+    elements[i].innerText = "Der Link wurde entfernt, Herr [name einfügen]." 
+    elements[i].style.fontWeight = "bold";
+    elements[i].style.color = "#822424";
+    
+  } else {
+    console.log("Keine Werte vorhanden!")
+  }
+}
