4c721c11e55034e58f1405ef4a5cf88d0a1ae7dd
Scott Zachariae <scott.zachariae@ibykus.de>
2024-06-18 16:02:51 +0200

    Changed

--- blocker.js
+++ blocker.js
@@ -6,7 +6,7 @@ for (var i = 0; i < elements.length; i++) {
   // console.log(elements[i])
 
   if (elements[i].innerText.includes("ringer Allgemeine")) {
-    elements[i].innerText = "Der Link wurde entfernt, Herr [name einfügen]." 
+    elements[i].innerText = "Der Eintrag wurde entfernt, Herr [name einfügen]." 
     elements[i].style.fontWeight = "bold";
     elements[i].style.color = "#822424";
     
