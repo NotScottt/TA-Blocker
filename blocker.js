336827b55948c33c6973e0de081bed84846543ff
Scott Zachariae <scott.zachariae@ibykus.de>
2024-06-19 15:31:55 +0200

    New

--- blocker.js
+++ blocker.js
@@ -1,16 +1,31 @@
-let elements = document.getElementsByClassName("SoAPf")
-
-// console.log(elements[0])
+let elements = document.getElementsByClassName("SoAPf");
+const final_items = []
 
 for (var i = 0; i < elements.length; i++) {
-  // console.log(elements[i])
+  let currentParent = elements[i].parentNode;
 
-  if (elements[i].innerText.includes("ringer Allgemeine")) {
-    elements[i].innerText = "Der Eintrag wurde entfernt, Herr [name einfügen]." 
-    elements[i].style.fontWeight = "bold";
-    elements[i].style.color = "#822424";
+  let string_from_item = currentParent.childNodes[1];
+  // let img_from_item = currentParent.childNodes[0]
+
+  try {
+    if (string_from_item.innerText.includes("ringer Allgemeine")) {
+
+      // console.log(string_from_item.parentNode.parentNode)
+      final_items.push(string_from_item.parentNode.parentNode)
+    }
     
-  } else {
-    console.log("Keine Werte vorhanden!")
+  } catch (err) {
+    console.log("Es gab folgenden Fehler bei den Elementen: " + err)
   }
+
+  //   final_items.push(string_from_item.parentNode.parentNode)
+  // }
+  // string_from_item.parentNode.remove()
+  // string_from_item.parentNode.parentNode.remove()
+}
+
+console.log(final_items)
+
+for (var i = 0; i < final_items.length; i++) {
+  final_items[i].remove()
 }
