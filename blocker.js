60fc1aa12e9c6c50442ce048c0c477c25ba70c0d
Scott Zachariae <scott.zachariae@ibykus.de>
2024-06-19 15:50:51 +0200

    Füge Highlight-Items hinzu

--- blocker.js
+++ blocker.js
@@ -1,31 +1,46 @@
 let elements = document.getElementsByClassName("SoAPf");
+let highlighted_items = document.getElementsByClassName("kMGqad");
+
+const highlighted_items_arr = []
 const final_items = []
 
+// Schleife für die normalen Items
 for (var i = 0; i < elements.length; i++) {
   let currentParent = elements[i].parentNode;
-
   let string_from_item = currentParent.childNodes[1];
-  // let img_from_item = currentParent.childNodes[0]
-
   try {
     if (string_from_item.innerText.includes("ringer Allgemeine")) {
-
-      // console.log(string_from_item.parentNode.parentNode)
       final_items.push(string_from_item.parentNode.parentNode)
+    }
+  } catch (err) {
+    console.log("Es gab folgenden Fehler bei den Elementen: " + err)
+  }
+}
+
+// Schleife für die Highlight Items
+for (var i = 0; i < highlighted_items.length; i++) {
+  let currentParent = highlighted_items[i].childNodes;
+  let string_for_highlight = currentParent[1]
+  try {
+    if (string_for_highlight.innerText.includes("ringer Allgemeine")) {
+      highlighted_items_arr.push(string_for_highlight.parentNode) 
     }
     
   } catch (err) {
     console.log("Es gab folgenden Fehler bei den Elementen: " + err)
   }
-
-  //   final_items.push(string_from_item.parentNode.parentNode)
-  // }
-  // string_from_item.parentNode.remove()
-  // string_from_item.parentNode.parentNode.remove()
 }
 
-console.log(final_items)
+// console.log(final_items)
+// console.log(highlighted_items_arr)
 
+// Schleife um die normalen Items zu entfernen 
 for (var i = 0; i < final_items.length; i++) {
   final_items[i].remove()
+}
+
+
+// Schleife um die highlight Items zu entfernen 
+for (var i = 0; i < highlighted_items_arr.length; i++) {
+  highlighted_items_arr[i].remove() 
 }
